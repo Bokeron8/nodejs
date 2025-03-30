674318321168c05fc6c0c0ac2929dcda6f39167d
Bokeron8 <67697305+Bokeron8@users.noreply.github.com>
2025-03-30 15:57:03 -0300

    api

--- routes/index.js
+++ routes/index.js
@@ -9,16 +9,34 @@ router.get('/', (req, res) => {
 });
 
 
-router.get('/proxy', async (req, res) => {
-  const targetUrl = 'https://menu.fu.do/tajyrooftopbar/qr-menu'; // Hardcoded for now
-
+router.get('/api/products', async (req, res) => {
   try {
-    const response = await axios.get(targetUrl);
-    res.send(response.data); // Or res.send(response.data) if it's not JSON
+    const response = await axios.get('https://integrations.fu.do/fudo/products', {
+      headers: {
+        Accept: 'application/json, text/plain, */*',
+        'Accept-Encoding': 'gzip, deflate, br, zstd',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Access-Control-Allow-Origin': '*',
+        Connection: 'keep-alive',
+        'Content-Type': 'application/json',
+        'fudo-account-id': 'dGFqeXJvb2Z0b3BiYXI=',
+        'fudo-app': 'qr-menu',
+        Origin: 'https://menu.fu.do',
+        Referer: 'https://menu.fu.do/',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-site',
+        TE: 'trailers',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:136.0) Gecko/20100101 Firefox/136.0'
+      }
+    });
+    
+    res.json(response.data);
   } catch (error) {
-    console.error('Proxy error:', error);
-    res.status(500).send('Proxy error');
+    console.error(error);
+    res.status(500).json({ error: 'Error al obtener los productos' });
   }
+
 });
 
 
