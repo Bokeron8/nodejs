5fca60ae11c7ab585cb3458d3798a1792be693a6
Bokeron8 <67697305+Bokeron8@users.noreply.github.com>
2025-03-30 15:49:25 -0300

    update

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const cors = require('cors'); 
 const path = require('path');
 const indexRouter = require('./routes/index');
 
 const app = express();
 const PORT = 3000;
-
+app.use(cors());
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
 const router = express.Router();
 
 // Serve the index.html file for the root route
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../views/index.html'));
 });
+
+
+router.get('/proxy', async (req, res) => {
+  const targetUrl = 'https://menu.fu.do/tajyrooftopbar/qr-menu'; // Hardcoded for now
+
+  try {
+    const response = await axios.get(targetUrl);
+    res.send(response.data); // Or res.send(response.data) if it's not JSON
+  } catch (error) {
+    console.error('Proxy error:', error);
+    res.status(500).send('Proxy error');
+  }
+});
+
 
 module.exports = router;
